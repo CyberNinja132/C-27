07dada7e363de24eb26f739f7cbb74cad01b5e1d
CyberNinja132 <lavya.k.himandi@gmail.com>
2021-07-22 15:30:15 +0530

    added new things, experiment

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +16,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,6 +36,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(log6.body,bird.body);
+
 
 }
 
@@ -61,4 +66,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
+
 }
